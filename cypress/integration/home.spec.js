d96fb7af2cfe39d4dc8372ad8b12c3f20565b1d0
Graham Fujisawa <sfrrki@gmail.com>
2023-03-14 16:10:14 -0700

    add testing in cypress for homapage

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+
+
+describe('Homepage', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:3000/')
+    // cy.contains("Looking for a way to add some life to your home?");
+  })
+  
+  it('Loads the homepage', () => {
+    cy.contains("Looking for a way to add some life to your home?");
+  })
+  it("There are products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  it("There are 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+})
